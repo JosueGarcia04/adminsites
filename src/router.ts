ea554b3f8c077702a52ddc7bff2eb3062de2acaf
JosueGarcia04 <josueadrian042006@gmail.com>
2025-01-04 01:12:19 -0600

    algunas validaciones en backend

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,15 @@
 import {Router} from 'express'
+import {body} from 'express-validator'
 import { createAccount } from './handlers';
 const router = Router()
 
 //sign in and auth
 
-router.post('/auth/register', createAccount );
+router.post('/auth/register',
+    body('handle').notEmpty().withMessage('el handle no puede ir vacio '),
+    body('name').notEmpty().withMessage('el nombre no puede ir vacio '), 
+    body('email').isEmail().withMessage('email incorrecto '), 
+    body('password').isLength({min: 8, max: 16}).withMessage('contraseña incorrecta'), 
+    createAccount );
 
 export default router
