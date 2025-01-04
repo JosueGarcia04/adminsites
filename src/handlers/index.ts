537b579dfc73f88d827f9bc905eab7aeba9485b0
JosueGarcia04 <josueadrian042006@gmail.com>
2025-01-04 00:46:31 -0600

    hash and handler

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,21 +1,26 @@
-import type {Request, Response} from 'express'
+import {Request, Response} from 'express'
+import slugify from 'slugify'
 import User from '../models/User'
 import { hashPassword } from '../utils/auth'
 
 export const createAccount = async (req: Request, res: Response) => {
-    //verificar existencia de usuarios
     const {email, password} = req.body
-    console.log(email)
+      //verificar existencia de usuarios
     const UserExist = await User.findOne({email})
     if(UserExist){
-       console.log("usuario existente")
-    }else{
-        console.log("usuario no existente ")
+        console.log("email no existente")
+    }
+    const handle = slugify(req.body.handle, '')
+
+    const HandleExist = await User.findOne({handle})
+    if(HandleExist){
+        console.log("nombre de usuario no disponible")
     }
 
     //creacion del usuario
     const user = new User(req.body)
     user.password= await hashPassword(password)
+    user.handle = handle
     await user.save()
-    res.send('registro exitoso')
+    res.status(201).send('registro exitoso')
 }
